5212027b572b233a1e86a90ca4d7be93dfa130a3
Taranjit Singh Bedi <85442736+taranbedi2000@users.noreply.github.com>
2023-02-14 20:22:55 +0530

    Add files via upload

---
+++ build/precache-manifest.eedab4e2a065b63c865296a4f5449975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2401d5441b61d6a930ad",
+    "url": "/static/css/main.38c3d58b.chunk.css"
+  },
+  {
+    "revision": "2401d5441b61d6a930ad",
+    "url": "/static/js/main.2401d544.chunk.js"
+  },
+  {
+    "revision": "8be06283ff8d9bfd4988",
+    "url": "/static/css/1.26254000.chunk.css"
+  },
+  {
+    "revision": "8be06283ff8d9bfd4988",
+    "url": "/static/js/1.8be06283.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6239a423a2670a3816f2144f1fc3f36",
+    "url": "/index.html"
+  }
+];
